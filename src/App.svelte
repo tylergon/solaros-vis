7cb6fe7a8925c3a08b83794a599481e0bf6d72a4
Tyler Goncalves <tylergoncalves@shaw.ca>
2026-07-22 16:29:29 -0700

    Switched to a flow using MIERUNE's svelte extension & Tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  // import './app.css'
   import type * as maplibregl from 'maplibre-gl';
   import {
     MapLibre,
@@ -19,20 +20,21 @@
 
 <!-- <PMTilesProtocol /> -->
 
-<!-- <section id="center">
+<section id="center">
   <div>
     <h1>Kamloops Rooftop Solar Resources</h1>
     <p>This tool is to let you explore and check out what we got!</p>
   </div>
-</section> -->
+</section>
 
-
-<MapLibre
-    class="h-[55vh] min-h-75"
-    style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-    zoom={3.5}
-    center={{ lng: 137, lat: 36 }}
->
-  <NavigationControl />
-  <ScaleControl />
-</MapLibre>
+<section id="map">
+  <MapLibre
+      class="h-[55vh] min-h-75"
+      style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+      zoom={3.5}
+      center={{ lng: 137, lat: 36 }}
+  >
+    <NavigationControl />
+    <ScaleControl />
+  </MapLibre>
+</section>
